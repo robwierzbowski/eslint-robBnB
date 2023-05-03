4e4713326429aa8ab79b3da263686392913ff1fe
Rob Wierzbowski <rob.wierzbowski@gmail.com>
2023-05-03 16:02:59 -0500

    Remove all non-shareable config from the repo

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-// eslint-disable-next-line import/no-namespace
-import * as typescriptParser from '@typescript-eslint/parser';
-import prettierConfig from 'eslint-config-prettier';
-import globals from 'globals';
-import { latestESGlobals } from './utils/index.js';
 import { coreRules } from './rules/core.js';
 import {
   importPluginConfig,
@@ -41,125 +36,64 @@ import {
   validateJsxNestingRules,
 } from './rules/validateJsxNesting.js';
 
-// Don't set more rules than we need to
-process.env.ESLINT_CONFIG_PRETTIER_NO_DEPRECATED = 'true';
-
-const languageOptions = {
-  globals: {
-    // Adding both browser and node globals for all files for convenience. Each
-    // project will have its own organization of browser files and node files
-    // and can override this setting if desired.
-    ...globals.browser,
-    ...globals.node,
-    ...latestESGlobals(globals),
+const javaScript = {
+  plugins: {
+    ...importPluginConfig,
+    ...jsxA11yPluginConfig,
+    ...preferArrowPluginConfig,
+    ...preferEarlyReturnPluginConfig,
+    ...reactHooksPluginConfig,
+    ...reactPreferFunctionComponentPluginConfig,
+    ...reactPluginConfig,
+    ...unicornPluginConfig,
+    ...validateJsxNestingPluginConfig,
   },
-  // Vite uses ESBuild to transpile JS, but ESBuild doesn't expose an AST we can
-  // use for ESLint. But, we can use the TS parser to parse 2020+ JS in both JS
-  // and TS files. The other option is to introduce the Babel parser, but that's
-  // too many tools doing the same job. Reference:
-  // https://github.com/evanw/esbuild/issues/1880
-  // https://github.com/swc-project/swc/issues/246
-  parser: typescriptParser,
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 'latest',
-    // Using the new JSX runtime auto insertion
-    // https://typescript-eslint.io/architecture/parser/#jsxpragma
-    jsxPragma: null,
-    project: './tsconfig.json',
-    sourceType: 'module',
-    tsconfigRootDir: '.',
+  rules: {
+    ...coreRules,
+    ...importRules,
+    ...jsxA11yRules,
+    ...preferArrowRules,
+    ...preferEarlyReturnRules,
+    ...reactHooksRules,
+    ...reactPreferFunctionComponentRules,
+    ...reactRules,
+    ...unicornRules,
+    ...validateJsxNestingRules,
+  },
+  settings: {
+    ...importSettings,
+    ...reactSettings,
   },
 };
 
-const config = [
-  {
-    ignores: ['dist/*'],
+const packageJson = {
+  plugins: {
+    ...packageJsonPluginConfig,
   },
-
-  // package.json file
-  {
-    files: ['package.json'],
-    plugins: {
-      ...packageJsonPluginConfig,
-    },
-    processor: packageJsonProcessor,
-    rules: {
-      ...packageJsonRules,
-    },
+  processor: packageJsonProcessor,
+  rules: {
+    ...packageJsonRules,
   },
+};
 
-  // JavaScript-based files
-  {
-    files: ['**/*.js', '**/*.ts', '**/*.tsx', '**/*.d.ts'],
-    languageOptions,
-    linterOptions: {
-      reportUnusedDisableDirectives: true,
-    },
-    plugins: {
-      ...importPluginConfig,
-      ...jsxA11yPluginConfig,
-      ...preferArrowPluginConfig,
-      ...preferEarlyReturnPluginConfig,
-      ...reactHooksPluginConfig,
-      ...reactPreferFunctionComponentPluginConfig,
-      ...reactPluginConfig,
-      ...unicornPluginConfig,
-      ...validateJsxNestingPluginConfig,
-    },
-    rules: {
-      ...coreRules,
-      ...importRules,
-      ...jsxA11yRules,
-      ...preferArrowRules,
-      ...preferEarlyReturnRules,
-      ...reactHooksRules,
-      ...reactPreferFunctionComponentRules,
-      ...reactRules,
-      ...unicornRules,
-      ...validateJsxNestingRules,
-    },
-    settings: {
-      ...importSettings,
-      ...reactSettings,
-    },
+const test = {
+  plugins: {
+    ...jestPluginConfig,
+    ...testingLibraryPluginConfig,
   },
-
-  // Test files
-  {
-    files: ['**/*.test.js', '**/*.test.ts', '**/*.test.tsx', '**/test/**'],
-    languageOptions: {
-      globals: {
-        ...languageOptions.globals,
-        ...globals.jest,
-      },
-    },
-    plugins: {
-      ...jestPluginConfig,
-      ...testingLibraryPluginConfig,
-    },
-    rules: {
-      ...jestRules,
-      ...testingLibraryRules,
-    },
+  rules: {
+    ...jestRules,
+    ...testingLibraryRules,
   },
+};
 
-  // TypeScript files
-  {
-    files: ['**/*.ts', '**/*.tsx', '**/*.d.ts'],
-    plugins: {
-      ...typescriptPluginConfig,
-    },
-    rules: {
-      ...typescriptRules,
-    },
+const typescript = {
+  plugins: {
+    ...typescriptPluginConfig,
   },
+  rules: {
+    ...typescriptRules,
+  },
+};
 
-  // Prettier config must be last disable any rules that conflict with its
-  // formatting
-  prettierConfig,
-];
-
-export { config };
+export { javaScript, packageJson, test, typescript };
