4d5edbce5e5dcc705ea55210ecbae30e8d09b2a0
Rob Wierzbowski <rob.wierzbowski@gmail.com>
2023-05-08 11:56:32 -0500

    Prevent TS config from throwing an error

---
+++ rules/empty.ts
@@ -0,0 +1,4 @@
+// TypeScript rules require a TS config to run, and a TS config requires at
+// least one TypeScript file in the repo. I may replace this in the future with
+// some .d.ts files to describe exports, but for now I just want a repo with no
+// errors.
