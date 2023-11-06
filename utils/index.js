04052743e9da0516a5706e6deeead7a08e100217
Rob Wierzbowski <rob.wierzbowski@gmail.com>
2023-11-05 19:53:31 -0600

    Better document the addPrefix utility function

--- utils/index.js
+++ utils/index.js
@@ -3,7 +3,11 @@ const ERROR = 'error';
 const OFF = 'off';
 const WARN = 'warn';
 
-// Applies a prefix/plugin name to all rules in an object
+// Most ESLint packages document and reference their rules without the required
+// prefix (e.g., `anchor-is-valid` instead of `jsx-a11y/anchor-is-valid`). In
+// order to easily copy/paste from package documentation, and to ensure
+// programmatically that the rule prefix matches the settings prefix, we'll add
+// prefixes to an object of rules with this utility function.
 const addPrefix = (prefix, unprefixedRules) =>
   Object.fromEntries(
     Object.entries(unprefixedRules).map(([key, value]) => [
